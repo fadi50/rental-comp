b79656f2aa1b0d65f648ebef24085b23f654dab3
fadi50 <adiboughanem45@gmail.com>
2025-02-28 22:40:48 +0200

    added another class called bike also inherites from vehicle and add some info about the bike

--- car rental/carrental.py
+++ car rental/carrental.py
@@ -28,3 +28,13 @@ class Car(Vehicle):
     def display_info(self):
         print(f"Car: {self.brand} {self.model}, Year: {self.year}, Seats: {self.seating_capacity}, Rental Price: ${self.get_rental_price_per_day()}/day")
     #i defined display_info method to display the brand,model,year,seating capacity and rental price of the car 
+class Bike(Vehicle):
+    def __init__(self, brand, model, year, rental_price_per_day, engine_capacity):
+        super().__init__(brand, model, year, rental_price_per_day)
+        self.engine_capacity = engine_capacity
+    
+    def display_info(self):
+        print(f"Bike: {self.brand} {self.model}, Year: {self.year}, Engine: {self.engine_capacity}cc, Rental Price: ${self.get_rental_price_per_day()}/day")
+
+def show_vehicle_info(vehicle):
+    vehicle.display_info()    
